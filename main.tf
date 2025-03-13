da7466638b924c349c151bf510dfc3d81a694881
Пулатов Иззатбек <pulatov.izzatbek@wb.ru>
2025-03-13 16:16:30 +0500

    ArgoCD module init

---
+++ main.tf
@@ -0,0 +1,46 @@
+terraform {
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.35.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "2.16.1"
+    }
+  }
+}
+provider "kubernetes" {
+  cluster_ca_certificate = base64decode(var.kubernetes_cluster_cert_data)
+  host                   = var.kubernetes_cluster_endpoint
+  exec {
+    api_version = "client.authentication.k8s.io/v1alpha1"
+    command     = "aws-iam-authenticator"
+    args        = ["token", "-i", var.kubernetes_cluster_name]
+  }
+}
+
+provider "helm" {
+  kubernetes {
+    cluster_ca_certificate = base64decode(var.kubernetes_cluster_cert_data)
+    host                   = var.kubernetes_cluster_endpoint
+    exec {
+      api_version = "client.authentication.k8s.io/v1alpha1"
+      command     = "aws-iam-authenticator"
+      args        = ["token", "-i", var.kubernetes_cluster_name]
+    }
+  }
+}
+
+resource "kubernetes_namespace" "example" {
+  metadata {
+    name = "argo"
+  }
+}
+
+resource "helm_release" "argocd" {
+  chart      = "argo-cd"
+  name       = "msur"
+  repository = "https://argoproj.github.io/argo-helm"
+  namespace  = kubernetes_namespace.example.metadata.0.name
+}
---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "kubernetes_cluster_id" {}
+
+variable "kubernetes_cluster_cert_data" {
+  type = string
+}
+
+variable "kubernetes_cluster_endpoint" {
+  type = string
+}
+
+variable "kubernetes_cluster_name" {
+  type = string
+}
+
+variable "eks_nodegroup_id" {
+  type = string
+}
